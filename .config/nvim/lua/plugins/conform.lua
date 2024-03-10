440db61fbb26d057f36a294fd0e5938fe69bac77
Exiles.prx <exiles.prx@gmail.com>
2024-03-10 12:22:48 -0400

    Formatters
    Adding more formatters for JS/TS support and others. Updating some
    plugins

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -6,9 +6,17 @@ return {
   opts = {
     ---@type table<string, conform.FormatterUnit[]>
     formatters_by_ft = {
-      lua = { "stylua" },
-      sh = { "shfmt" },
-      php = { "phpcs" },
+      ["lua"] = { "stylua" },
+      ["sh"] = { "shfmt" },
+      ["php"] = { "phpcs" },
+      ["javascript"] = { "prettier" },
+      ["typescript"] = { "prettier" },
+      ["vue"] = { "prettier" },
+      ["css"] = { "prettier" },
+      ["html"] = { "prettier" },
+      ["json"] = { "prettier" },
+      ["yaml"] = { "prettier" },
+      ["markdown"] = { "prettier" },
     },
     -- The options you set here will be merged with the builtin formatters.
     -- You can also define any custom formatters here.
