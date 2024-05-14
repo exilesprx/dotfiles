1d9d53f2c935c98b2b1dfe5bc6e884839ce0fe91
Exiles.prx <exiles.prx@gmail.com>
2024-05-13 21:51:24 -0400

    Lazy updates

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,8 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<leader>t", ":TestNearest<CR>")
-vim.keymap.set("n", "<leader>T", ":TestFile<CR>")
-vim.keymap.set("n", "<leader>a", ":TestSuite<CR>")
-vim.keymap.set("n", "<leader>l", ":TestLast<CR>")
-vim.keymap.set("n", "<leader>g", ":TestVisit<CR>")
+vim.keymap.set("n", "<leader>tn", ":TestNearest<CR>")
+vim.keymap.set("n", "<leader>tf", ":TestFile<CR>")
+vim.keymap.set("n", "<leader>ts", ":TestSuite<CR>")
+vim.keymap.set("n", "<leader>tl", ":TestLast<CR>")
+vim.keymap.set("n", "<leader>tv", ":TestVisit<CR>")
