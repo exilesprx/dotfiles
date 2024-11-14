c6ecb31ff4a74d1f383dad95570ac5a464eb4266
Exiles.prx <exiles.prx@gmail.com>
2024-11-13 19:09:59 -0500

    Lazy updates

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.php" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.vue" },
+    { import = "lazyvim.plugins.extras.lang.zig" },
     { import = "lazyvim.plugins.extras.test.core" },
     -- import/override with your plugins
     { import = "plugins" },
--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -4,12 +4,5 @@ return {
     opts = {
       inlay_hints = { enabled = true },
     },
-    init = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.zls.setup({
-        type = "zls",
-        cmd = { "zls" },
-      })
-    end,
   },
 }
