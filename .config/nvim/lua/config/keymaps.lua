2387c6517497cc2b39142b55561d144fc18804ab
Exiles.prx <exiles.prx@gmail.com>
2024-12-10 20:15:32 -0500

    Use whichkey
    Update key mappings to use which key

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
-vim.keymap.set("n", "<leader>zl", ":.lua<CR>", { desc = "Source line" })
-vim.keymap.set("n", "<leader>zf", "<cmd>source %<CR>", { desc = "Source file" })
-print("Keymaps loaded")
+local wk = require("which-key")
+wk.add({
+  { "<leader>z", group = "Lua" }, -- group
+  { "<leader>zl", ":.lua<CR>", desc = "Source line", mode = "n" },
+  { "<leader>zf", "<cmd>source %<CR>", desc = "Source file", mode = "n" },
+})
