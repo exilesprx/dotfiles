195eaf4700d6cb822cb4ed7ebea638261741a86c
Exiles.prx <exiles.prx@gmail.com>
2024-12-10 19:51:29 -0500

    Add new keymaps
    Ability to execute lua via keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>zl", ":.lua<CR>", { desc = "Source line" })
+vim.keymap.set("n", "<leader>zf", "<cmd>source %<CR>", { desc = "Source file" })
+print("Keymaps loaded")
