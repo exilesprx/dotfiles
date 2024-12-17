bea0f836ecb8cf21779cc05f5fc37e97dbeb292d
Exiles.prx <exiles.prx@gmail.com>
2024-12-16 22:03:30 -0500

    Key map updates
    Move lua key maps to whick key plugin file

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,9 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-local wk = require("which-key")
-wk.add({
-  { "<leader>z", group = "Lua" }, -- group
-  { "<leader>zl", ":.lua<CR>", desc = "Source line", mode = "n" },
-  { "<leader>zf", "<cmd>source %<CR>", desc = "Source file", mode = "n" },
-})
--- .config/nvim/lua/plugins/whick-key.lua
+++ .config/nvim/lua/plugins/whick-key.lua
@@ -3,6 +3,11 @@ return {
     "folke/which-key.nvim",
     opts = {
       preset = "classic",
+      spec = {
+        { "<leader>z", group = "Lua" },
+        { "<leader>zl", ":.lua<CR>", desc = "Source line", mode = "n" },
+        { "<leader>zf", "<cmd>source %<CR>", desc = "Source file", mode = "n" },
+      },
     },
   },
 }
