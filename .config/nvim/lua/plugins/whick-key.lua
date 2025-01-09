784722fb1b2c186b597eebcdefbecd498fb8fe28
Exiles.prx <exiles.prx@gmail.com>
2025-01-08 21:11:00 -0500

    Oil replaces neo-tree

--- .config/nvim/lua/plugins/whick-key.lua
+++ .config/nvim/lua/plugins/whick-key.lua
@@ -7,6 +7,7 @@ return {
         { "<leader>z", group = "Lua" },
         { "<leader>zl", ":.lua<CR>", desc = "Source line", mode = "n" },
         { "<leader>zf", "<cmd>source %<CR>", desc = "Source file", mode = "n" },
+        { "<leader>e", "<cmd>Oil<CR>", desc = "Open current directory", mode = "n" },
       },
     },
   },
