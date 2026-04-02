f7751e81c71e4e969cba510d51aba216ee761fff
Exiles.prx <exiles.prx@gmail.com>
2026-04-02 12:51:00 -0400

    fix(fmt): remove formatters

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -3,21 +3,4 @@ return {
   dependencies = { "mason.nvim" },
   lazy = true,
   cmd = "ConformInfo",
-  opts = {
-    ---@type table<string, conform.FormatterUnit[]>
-    formatters_by_ft = {
-      ["bash"] = { "shellcheck" },
-      ["css"] = { "prettier" },
-      ["html"] = { "prettier" },
-      ["javascript"] = { "prettier" },
-      ["json"] = { "prettier" },
-      ["lua"] = { "stylua" },
-      ["markdown"] = { "prettier" },
-      ["php"] = { "php_cs_fixer" },
-      ["typescript"] = { "prettier" },
-      ["sh"] = { "shfmt" },
-      ["vue"] = { "prettier" },
-      ["yaml"] = { "prettier" },
-    },
-  },
 }
