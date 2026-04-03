af024b41948b873959667d6ed4defc5d7012e24a
Exiles.prx <exiles.prx@gmail.com>
2026-04-03 12:01:57 -0400

    feat: oxfmt support/toggle

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -3,4 +3,18 @@ return {
   dependencies = { "mason.nvim" },
   lazy = true,
   cmd = "ConformInfo",
+  opts = {
+    formatters = {
+      oxfmt = {
+        condition = function(_, ctx)
+          return vim.fs.find({ ".oxfmtrc.json", ".oxfmtrc.jsonc" }, { path = ctx.filename, upward = true })[1]
+        end,
+      },
+      prettier = {
+        condition = function(_, ctx)
+          return vim.fs.find({ ".prettierrc.json" }, { path = ctx.filename, upward = true })[1]
+        end,
+      },
+    },
+  },
 }
