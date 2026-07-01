b4f1bfef6d147f8f77b89b599d61178d495b144b
Exiles.prx <exiles.prx@gmail.com>
2026-07-01 14:36:58 -0400

    feat: opencode integration
    remove copilot chat in favor of opencode integration

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -1,10 +1,11 @@
 return {
   {
     "saghen/blink.cmp",
-    opts = {
-      keymap = {
-        preset = "default",
-      },
-    },
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      opts.sources.per_filetype = vim.tbl_deep_extend("force", opts.sources.per_filetype or {}, {
+        opencode_ask = { "lsp", "buffer" },
+      })
+    end,
   },
 }
--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -3,6 +3,10 @@ return {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
       opts.options.theme = "palenight"
+      opts.sections = opts.sections or {}
+      opts.sections.lualine_z = vim.list_extend(opts.sections.lualine_z or {}, {
+        { require("opencode").statusline },
+      })
     end,
   },
 }
