8e513d8c51e72be6ddd4bd0ec9dbefad7bf93a3b
Exiles.prx <exiles.prx@gmail.com>
2026-07-02 17:23:01 -0400

    feat: mini files

---
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "folke/which-key.nvim",
+    opts = {
+      preset = "classic",
+      spec = {
+        { "<leader>z", group = "Lua" },
+        { "<leader>zl", ":.lua<CR>", desc = "Source line", mode = "n" },
+        { "<leader>zf", "<cmd>source %<CR>", desc = "Source file", mode = "n" },
+        -- { "<leader>e", "<cmd>Oil<CR>", desc = "Open current directory", mode = "n" },
+        {
+          "<leader>e",
+          function()
+            require("mini.files").open(vim.uv.cwd(), true)
+          end,
+          desc = "Open mini.files (cwd)",
+        },
+        {
+          "<leader>E",
+          function()
+            require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+          end,
+          desc = "Open mini.files (directory of current file)",
+        },
+      },
+    },
+  },
+}
